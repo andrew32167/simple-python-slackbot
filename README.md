428c7e304d6635173b9a4f3d9a7afb81804e8d25
Andrey Rimovych <ar@ar.local>
2017-04-20 17:55:11 +0300

    added example of polling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # simple-python-slackbot
 Simple to start and maintain your personal slackbot
+The main stuff is Bot.py class that simplifies slackbot integration
+# Usage
+Just import Bot class and pass `token` and `bot_id` to constructor and you're ready to go
+```
+from Bot import Bot
+    
+bot = Bot(bot_token='YOUR_BOT_TOKEN',
+          bot_id='YOUR_BOT_ID')
+          
+
+```
--- examples/poll.py
+++ examples/poll.py
@@ -1,16 +1,22 @@
 
 import os
-from Bot import Bot, CommandHandler
+import sys
 import re
+sys.path.append('..')
+from Bot import Bot, CommandHandler
 
 
 class MyHandler(CommandHandler):
-    _bot = None
+    '''
+    Custom handler to process @cluster messages
+
+    '''
 
     def handle_command(self, command, channel, user):
         command = re.sub(' +', ' ', command)
-        if command.startswith('what\'s up'):
-            self._bot.post_message(channel=channel, message="@{} all good".format(user))
+
+        if command.startswith('hi'):
+            self._bot.post_message(channel=channel, message="Hi {}".format(user))
             question = 'Would you like some coffee?'
             answers = ['yes', 'no']
             answer = self._bot.wait_for_answer_from_user(question, answers, channel, user, new_thread=True)
@@ -21,15 +27,21 @@ class MyHandler(CommandHandler):
 
 
 def main():
+    '''
+    Example of using bot class to poll and answer commands
 
+    :return:
+    '''
     bot = Bot(bot_token=os.environ.get('CLUSTER_BOT_TOKEN'),
               bot_id=os.environ.get('CLUSTER_BOT_ID'),
               command_handler=MyHandler)
 
     chnl = 'D39TY1ZV2'
     msg = bot.post_message(chnl, 'I\'m here bitchez!!')
-    bot.add_reaction(msg, "facepalm", chnl)
+    bot.add_reaction(msg, "slightly_smiling_face", chnl)
 
     bot.poll(non_stop=False)
 
-main()
+
+if __name__ == '__main__':
+    main()
